ee5fafebf5a678e6ff7fd3738e6e7659aecd2456
milkon <melkon_2010@hotmail.com>
2021-05-06 21:24:47 +0200

    axios config

--- src/apis/youTube.js
+++ src/apis/youTube.js
@@ -1,1 +1,12 @@
+import axios from 'axios';
+
 const KEY = 'AIzaSyA79DSJjGxRT4FfhnmtNZmy93dnXVFFVr0';
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    maxResults: 5,
+    key: KEY,
+  },
+});
