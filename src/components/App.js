64e7a9679612cc698229a145504efaf19f534fed
milkon <melkon_2010@hotmail.com>
2021-05-07 12:00:20 +0200

    added VideoDetail component

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youTube';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 class App extends React.Component {
   state = {
@@ -20,13 +21,14 @@ class App extends React.Component {
   };
 
   onVideoSelect = video => {
-    console.log('From the APP!', video);
+    this.setState({ selectedVideo: video });
   };
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onFromSubmit={this.onTermSubmit} />
+        <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           onVideoSelect={this.onVideoSelect}
           videos={this.state.videos}
