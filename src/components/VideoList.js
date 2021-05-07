2635da003ed9d2c36356cdc9b5ba798203c0ddc2
milkon <melkon_2010@hotmail.com>
2021-05-07 12:18:25 +0200

    added selecting video + video player functionality

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -3,7 +3,13 @@ import VideoItem from './VideoItem';
 
 const VideoList = ({ videos, onVideoSelect }) => {
   const renderedList = videos.map(video => {
-    return <VideoItem onVideoSelect={onVideoSelect} video={video} />;
+    return (
+      <VideoItem
+        onVideoSelect={onVideoSelect}
+        video={video}
+        key={video.id.videoId}
+      />
+    );
   });
   return <div className="ui relaxed divided lis">{renderedList}</div>;
 };
